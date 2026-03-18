5f75b57c97455415eedb2fde6e39ac71244a83a3
Isaias <isasalas145@gmail.com>
2026-03-18 09:41:19 -0400

    New Update March 2026

--- src/controllers/eventoObs.controller.ts
+++ src/controllers/eventoObs.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { EventoObsModel } from "../models/eventoObs.model.js";
+import { ObsModel } from "../models/obs.model.js";
 
 export async function getEventoObs(req: Request, res: Response) {
   const { id_evento } = req.params;
@@ -7,8 +8,8 @@ export async function getEventoObs(req: Request, res: Response) {
   try {
     const TempEventoObs = await EventoObsModel.findAll({
       where: { id_evento },
-
       order: [["id", "DESC"]],
+      include: [{ model: ObsModel, paranoid: false, attributes: ["id", "name"] }],
     });
     res.status(200).json(TempEventoObs);
   } catch (error) {
